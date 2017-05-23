59b7eccb24f259214deb8671803ee0288048e612
IlyaDerendyaev <gamesky27@gmail.com>
2017-05-23 21:29:10 +0500

    Update Main.py (2)
    Убрал ограничение запросов

--- Main.py
+++ Main.py
@@ -1,34 +1,21 @@
-
 import requests
 from graphviz import Digraph
 import module_commiters
 import module_projects
-
 
 f = open('input.txt')
 str = f.read()
 f.close()
 users = str.split(' ')
 
-'''
-users = project_commiters('r1chardj0n3s/parse')
-print(users)
-'''
-
 dot = Digraph(comment='Collaborations', format='png')
 
 for user in users:
 	dot.node(user, user)
 
-
-
 for user in users:
 	user_repos = module_projects.user_projects(user)
-	i = 0
-	n = min(len(user_repos), 5)
-	while i < n:
-		repo = user_repos[i]
-		i = i + 1
+	for repo in user_repos:
 		dot.node(repo, repo)
 		commiters = module_commiters.project_commiters(repo)
 		result = list(set(commiters) & set(users))
